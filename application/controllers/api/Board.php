327ed47a687348040eba72e0379a94354a0da765
In8989 <kci3472@gmail.com>
2020-06-15 00:24:52 +0900

    20-06-15 api board token값 있을 때 기능하게

--- application/controllers/api/Board.php
+++ application/controllers/api/Board.php
@@ -8,15 +8,16 @@ class Board extends RestController
 {
 	private $is_auth;
 	private $jwt_auth;
-	private $objOfJwt;
 
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->database();
-		$this->objOfJwt = new Jwt_creator();
 		$this->jwt_auth = new Auth_creator();
 		$this->load->library('form_validation');
+		$received_Token = $this->input->get_request_header('Token');
+		$this->is_auth = $this->jwt_auth->auth_get($received_Token);
+//		print_r($this->is_auth);
 
 //		$this->output->enable_profiler(true);
 	}
@@ -26,15 +27,16 @@ class Board extends RestController
 		if($type == 'list') {
 			$this->load->library('pagination');
 
-			$config['base_url'] = '/api/ci_board/page';
-			$config['total_rows'] = '/api/ci_board/page';
-			$config['per_page'] = '/api/ci_board/page';
-			$config['uri_segment'] = 5;
-
-			$this->pagination->initialize($config);
+			$config['base_url'] = '/api/board/list/page';
+			$config['total_rows'] = $this->db->count_all('ci_board');
+			$config['per_page'] = 5;
+			$config['uri_segment'] = 3;
+			$limit = $config['per_page'];
+			// ((uri페이지) - 1 ) * $config['per_page']
+			$offset = ($this->uri->segment(4)-1)*$config['per_page'];
 
 			$this->db->order_by('board_id','desc');
-			$get_board = $this->db->get('ci_board');
+			$get_board = $this->db->get('ci_board',$limit,$offset);
 			$get_board->num_rows();
 			$result = $get_board->result_array();
 
@@ -43,6 +45,14 @@ class Board extends RestController
 			} else {
 				$this->response('No record found', 404);
 			}
+		}else if ($type == 'view'){
+			$board_id = $this->uri->segment(4);
+			$this->db->where('board_id',$board_id);
+			$get_view = $this->db->get('ci_board');
+			$get_view->num_rows();
+			$result = $get_view->row();
+			print_r($result);
+
 		}
 
 	}
@@ -50,22 +60,26 @@ class Board extends RestController
 	public function index_post($type)
 	{
 		if($type == 'write') {
-			$table = 'ci_board';
-			$user_id = $this->post('user_id');
-			$user_name = $this->post('user_name');
-			$subject = $this->post('subject');
-			$contents = $this->post('contents');
+			if(!$this->is_auth){
+				echo '로그인하세요';
+			}else {
 
-			$data['user_id'] = $user_id;
-			$data['user_name'] = $user_name;
-			$data['subject'] = $subject;
-			$data['contents'] = $contents;
+				$user_id = $this->post('user_id');
+				$user_name = $this->post('user_name');
+				$subject = $this->post('subject');
+				$contents = $this->post('contents');
 
-			$insert_board = $this->db->insert($table, $data);
-			if ($insert_board) {
-				$this->response('게시글작성완료', 200);
-			} else {
-				$this->response('작성실패', 404);
+				$data['user_id'] = $user_id;
+				$data['user_name'] = $user_name;
+				$data['subject'] = $subject;
+				$data['contents'] = $contents;
+
+				$insert_board = $this->db->insert('ci_board', $data);
+				if ($insert_board) {
+					$this->response('게시글작성완료', 200);
+				} else {
+					$this->response('작성실패', 404);
+				}
 			}
 		}
 
@@ -73,12 +87,35 @@ class Board extends RestController
 
 	public function index_put()
 	{
+		if(!$this->is_auth){
+			echo '로그인해요';
+		}else{
 
+			$data['board_id'] = $this->uri->segment(3);
+			$data['subject'] = $this->put('subject');
+			$data['contents'] = $this->put('contents');
+
+			if($data) {
+				$this->db->where('board_id', $data['board_id']);
+				$this->db->update('ci_board', $data);
+				echo '게시글이 수정되었습니다.';
+			}else{
+				echo 'data:false';
+			}
+		}
 	}
 
 	public function index_delete()
 	{
+		if(!$this->is_auth) {
+			echo '로그인해요';
+		}else{
+			$data['board_id'] = $this->uri->segment(3);
+			$this->db->where('board_id', $data['board_id']);
+			$this->db->delete('ci_board', $data);
 
+			echo $data['board_id'].'글이 삭제되었습니다.';
+		}
 	}
 
 }
